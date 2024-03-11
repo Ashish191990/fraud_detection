dbff1296c6c38536dca1211596fcb8d94e449371
Ashish191990 <crazy.maddy1990@gmail.com>
2024-03-11 18:24:30 +0530

    done

--- data.processing.py
+++ data.processing.py
@@ -3,7 +3,13 @@ print("adding all the data processing code related fraud detection")
 
 print (" this was done and saved. It should be visible on github now")
 
+a = 5
+b = 9
 
+def sum (a,b):
+    c = a+b
+    return c
+print ("added new function sum")
 
 
 
